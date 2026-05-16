7fdac0388c95c362ebb71649ea76b2f88d8e19b8
Casey Doran <caseyd.doran@gmail.com>
2026-05-16 10:43:05 -0700

    swap to binary qemu from upstream @10.1.5-pebble5
    short story:
    the version of qemu pinned here was really old because coredevices was shiping a VERY old qemu (duh, they were dead for ten years).
    in the time since this flake was written, coredevices rebased to a much more modern qemu (yay!) which changed to meson build. nixpkgs' meson build hooks are pretty hairy for me to unravel, so just patch the release bins for now. i'd like to come back later and see if we can get the package building from source, of course.

--- qemu-pebble/default.nix
+++ qemu-pebble/default.nix
@@ -1,73 +1,55 @@
 {
   lib,
   stdenv,
-  fetchFromGitHub,
-  gnumake,
-  python3,
-  pkg-config,
-  zlib,
+  fetchurl,
+  autoPatchelfHook,
+  SDL2,
   glib,
+  zlib,
   pixman,
-  dtc,
+  udev,
+  alsa-lib,
+  libpulseaudio,
+  sndio,
+  bzip2,
 }:
-let
-  libraries = [
-    zlib
-    glib
-    pixman
-    dtc
-    stdenv.cc.cc.lib
-  ];
-  rpath = lib.makeLibraryPath libraries;
-in
 stdenv.mkDerivation {
   pname = "qemu-pebble";
-  version = "73b3a52";
+  version = "10.1.5-pebble5";
 
-  src = fetchFromGitHub {
-    owner = "coredevices";
-    repo = "qemu";
-    rev = "73b3a52e8077adcfaf2005adb768e8f4e8a7bc74";
-    hash = "sha256-HX/q09yB40fJzm4bxAOgNBsfwgroFPejSqOr/lVlzgs=";
+  src = fetchurl {
+    url = "https://github.com/coredevices/qemu/releases/download/v10.1.5-pebble5/qemu-pebble-linux-x86_64.tar.gz";
+    hash = "sha256-yo171+UUg7h57EuGlDbcPVwipF3N9PPDV/ajSaIDztw=";
   };
 
-  enableParallelBuilding = true;
+  nativeBuildInputs = [ autoPatchelfHook ];
 
-  nativeBuildInputs = [
-    gnumake
-    python3
-    pkg-config
-  ]
-  ++ libraries;
-
-  LD_LIBRARY_PATH = rpath;
-
-  patches = [
-    # Removes the need for Python 2
-    # (it was only used in the build system)
-    ./move-to-python3.patch
+  buildInputs = [
+    SDL2
+    glib
+    zlib
+    pixman
+    udev
+    alsa-lib
+    libpulseaudio
+    sndio
+    bzip2.out
   ];
 
-  configureFlags = [
-    "--disable-werror"
-    "--enable-debug"
-    "--target-list=arm-softmmu"
-    "--extra-cflags=-DSTM32_UART_NO_BAUD_DELAY"
-  ];
-
-  # TODO: Is this really needed?
-  postFixup = ''
-    patchelf --set-rpath "${rpath}" $out/bin/qemu-system-arm
-  '';
-
-  postInstall = ''
-    ln -s $out/bin/qemu-system-arm $out/bin/qemu-pebble
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    cp qemu-pebble $out/bin/
+    chmod +x $out/bin/qemu-pebble
+    runHook postInstall
   '';
 
   meta = {
     homepage = "https://github.com/coredevices/qemu";
-    description = "Fork of QEMU for Pebble watches";
+    description = "Fork of QEMU for Pebble watches (pre-built)";
     mainProgram = "qemu-pebble";
-    platforms = lib.platforms.linux ++ lib.platforms.darwin;
+    platforms = [ "x86_64-linux" ];
+    license = lib.licenses.gpl2Plus;
+    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
   };
 }
